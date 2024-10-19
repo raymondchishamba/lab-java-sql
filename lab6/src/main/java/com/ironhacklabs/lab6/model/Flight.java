d33d7379a238fc573c3b4115eaf8205355168004
raymondchishamba <ray.chishamba@gmail.com>
2024-10-19 13:27:30 +0200

    Lab6

---
+++ lab6/src/main/java/com/ironhacklabs/lab6/model/Flight.java
@@ -0,0 +1,57 @@
+package com.ironhacklabs.lab6.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer flightId;
+    private String flightNumber;
+    private String aircraft;
+    private Integer totalAircraftSeats;
+    private Integer flightMileage;
+    public Flight() {
+    }
+    public Integer getFlightId() {
+        return flightId;
+    }
+    public void setFlightId(Integer flightId) {
+        this.flightId = flightId;
+    }
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+    public String getAircraft() {
+        return aircraft;
+    }
+    public void setAircraft(String aircraft) {
+        this.aircraft = aircraft;
+    }
+    public Integer getTotalAircraftSeats() {
+        return totalAircraftSeats;
+    }
+    public void setTotalAircraftSeats(Integer totalAircraftSeats) {
+        this.totalAircraftSeats = totalAircraftSeats;
+    }
+    public Integer getFlightMileage() {
+        return flightMileage;
+    }
+    public void setFlightMileage(Integer flightMileage) {
+        this.flightMileage = flightMileage;
+    }
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "flightId=" + flightId +
+                ", flightNumber='" + flightNumber + '\'' +
+                ", aircraft='" + aircraft + '\'' +
+                ", totalAircraftSeats=" + totalAircraftSeats +
+                ", flightMileage=" + flightMileage +
+                '}';
+    }
+}
